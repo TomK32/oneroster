206f68d629a481a48828b4b939a567c0bd6b4a25
Thomas R. Koll <tomk32@tomk32.de>
2016-11-17 14:34:19 +0100

    Add all the ignore_root options to endpoints

--- lib/oneroster/endpoints.rb
+++ lib/oneroster/endpoints.rb
@@ -1,55 +1,55 @@
 module Oneroster
   # https://oneroster.infinitecampus.org/campus/oneroster/entropyMaster/learningdata/v1
   class Administrator < Oneroster::ApiBase
-    get :all, "/administrators"
+    get :all, "/administrators", ignore_root: 'users'
   end
   class Category < Oneroster::ApiBase
-    get :all, "/categories"
+    get :all, "/categories", ignore_root: 'categories'
   end
   class Class < Oneroster::ApiBase
-    get :all, "/classes"
+    get :all, "/classes", ignore_root: 'classes'
   end
   class Course < Oneroster::ApiBase
-    get :all, "/courses"
+    get :all, "/courses", ignore_root: 'courses'
   end
   class District < Oneroster::ApiBase
     get :all, "/districts", ignore_root: 'orgs'
   end
   class Enrollment < Oneroster::ApiBase
-    get :all, "/enrollments"
+    get :all, "/enrollments", ignore_root: 'enrollments'
   end
   class GradingPeriod < Oneroster::ApiBase
-    get :all, "/gradingPeriods"
+    get :all, "/gradingPeriods", ignore_root: 'academicSession'
   end
   class Guardian < Oneroster::ApiBase
-    get :all, "/guardians"
+    get :all, "/guardians", ignore_root: 'users'
   end
   class Lineitem < Oneroster::ApiBase
-    get :all, "/lineitems"
+    get :all, "/lineitems", ignore_root: 'lineitems'
   end
   class Result < Oneroster::ApiBase
-    get :all, "/results"
+    get :all, "/results", ignore_root: 'results'
   end
   class School < Oneroster::ApiBase
-    get :all, "/schools"
+    get :all, "/schools", ignore_root: 'orgs'
   end
   class Scoregroup < Oneroster::ApiBase
-    get :all, "/scoreGroups"
+    get :all, "/scoreGroups", ignore_root: 'scoreGroups'
   end
   class Student < Oneroster::ApiBase
-    get :all, "/students"
-    get :find, "/student/:id"
+    get :all, "/students", ignore_root: 'users'
+    get :find, "/student/:id", ignore_root: 'users'
   end
   class Teacher < Oneroster::ApiBase
-    get :all, "/teachers"
-    get :find, "/teacher/:id"
+    get :all, "/teachers", ignore_root: 'users'
+    get :find, "/teacher/:id", ignore_root: 'user'
   end
   class Term < Oneroster::ApiBase
-    get :all, "/terms"
-    get :find, "/terms/:id"
+    get :all, "/terms", ignore_root: 'academicSessions'
+    get :find, "/terms/:id", ignore_root: 'academicSession'
   end
   class User < Oneroster::ApiBase
-    get :all, "/users"
-    get :find, "/user/:id"
+    get :all, "/users", ignore_root: 'users'
+    get :find, "/user/:id", ignore_root: 'users'
   end
 end
